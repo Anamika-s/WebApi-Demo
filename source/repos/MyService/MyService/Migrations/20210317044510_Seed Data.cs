d201731cdc0bb0e2619d9c8495221c86d573b2b6
Anamika <anamika.sawhney22@gmail.com>
2021-03-17 16:48:26 +0530

    MyService

---
+++ source/repos/MyService/MyService/Migrations/20210317044510_Seed Data.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyService.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Dept = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Salary = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_employees", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "employees",
+                columns: new[] { "Id", "Dept", "Name", "Salary" },
+                values: new object[] { 1, "HR", "Ajay", 90000 });
+
+            migrationBuilder.InsertData(
+                table: "employees",
+                columns: new[] { "Id", "Dept", "Name", "Salary" },
+                values: new object[] { 2, "Accts", "Deepak", 98000 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "employees");
+        }
+    }
+}
---
+++ source/repos/MyService/MyService/Models/Employee.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyService.Models
+{
+    public class Employee
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Dept { get; set; }
+        public int Salary { get; set; }
+    }
+    public class EmployeeDbContext : DbContext
+    {
+        public  EmployeeDbContext(DbContextOptions<EmployeeDbContext> options)
+            : base(options) { }
+        public DbSet<Employee> employees { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().
+                HasData(new Employee
+                {
+                    Id = 1,
+                    Name = "Ajay",
+                    Dept = "HR",
+                    Salary = 90000
+                }, new Employee
+                {
+                    Id = 2,
+                    Name = "Deepak",
+                    Dept = "Accts",
+                    Salary = 98000
+                }
+                );
+
+
+        }
+
+
+    }
+
+}
